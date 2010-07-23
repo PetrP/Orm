4b7d54fa93b50e6c46e47cea17b578bffa070523
PetrP <prochazkapp@gmail.com>
2010-07-23 18:37:52 +0200

    Tests: Compare rychlost (s doctrine2)

---
+++ tests/compare/d/entity/Person.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @Entity 
+ * @Table(name="persons")
+ */
+class PersonD
+{
+	/**
+	 * @Column(type="integer")
+	 * @Id
+	 */
+	private $id;
+
+	/** @Column(type="string") */
+	private $name;
+
+	/** @Column(type="integer") */
+	private $age;
+
+	/** @Column(type="string") */
+	private $letter;
+}
