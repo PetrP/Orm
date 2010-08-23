d71d36bfb183d4d5869b306f3a9b70b94805aeb6
PetrP <prochazkapp@gmail.com>
2010-08-23 07:47:23 +0200

    Validation: isEmail update

--- Model/Entity/ValidationHelper.php
+++ Model/Entity/ValidationHelper.php
@@ -71,13 +71,19 @@ class ValidationHelper
 
 	}
 
-	public static function isEmail($s)
+	/**
+	 * Email validator: is value valid email address?
+	 * @param  string
+	 * @return bool
+	 * @author David Grudl
+	 */
+	public static function isEmail($value)
 	{
 		$atom = "[-a-z0-9!#$%&'*+/=?^_`{|}~]"; // RFC 5322 unquoted characters in local-part
-		$localPart = "(\"([ !\\x23-\\x5B\\x5D-\\x7E]*|\\\\[ -~])+\"|$atom+(\\.$atom+)*)"; // quoted or unquoted
+		$localPart = "(?:\"(?:[ !\\x23-\\x5B\\x5D-\\x7E]*|\\\\[ -~])+\"|$atom+(?:\\.$atom+)*)"; // quoted or unquoted
 		$chars = "a-z0-9\x80-\xFF"; // superset of IDN
-		$domain = "[$chars]([-$chars]{0,61}[$chars])"; // RFC 1034 one domain component
-		return (bool) preg_match("(^$localPart@($domain?\\.)+[a-z]{2,14}\\z)i", $s); // strict top-level domain
+		$domain = "[$chars](?:[-$chars]{0,61}[$chars])"; // RFC 1034 one domain component
+		return (bool) preg_match("(^$localPart@(?:$domain?\\.)+[-$chars]{2,19}\\z)i", $value);
 	}
 
 }
