0e7fe2f2b8693b370d72dbcc976accd3a7be49ab
PetrP <prochazkapp@gmail.com>
2010-11-18 05:31:49 +0100

    Entity: event kontroluje jestli je vola spravny event

--- Model/Entity/_EntityEvent.php
+++ Model/Entity/_EntityEvent.php
@@ -13,63 +13,63 @@ abstract class _EntityEvent extends _EntityMeta
 	/** Vytvorena nova entita */
 	protected function onCreate()
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onCreate';
 	}
 
 	/** Vytazena z mapperu */
 	protected function onLoad(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onLoad';
 	}
 
 	/** Pred persistovanim (insert nebo update) */
 	protected function onBeforePersist(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onBeforePersist';
 	}
 	/** Po persistovani (insert nebo update) */
 	protected function onAfterPersist(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onAfterPersist';
 	}
 
 	/** Behem persistovani, vsechny subentity nemusi byt jeste persistovany */
 	protected function onPersist(IRepository $repository, $id)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onPersist';
 	}
 
 	/** Pred vymazanim */
 	protected function onBeforeDelete(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onBeforeDelete';
 	}
 	/** Po vymazani */
 	protected function onAfterDelete(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onAfterDelete';
 	}
 
 	/** Persistovane zmeny (update) */
 	protected function onBeforeUpdate(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onBeforeUpdate';
 	}
 	/** Persistovane zmeny (update) */
 	protected function onAfterUpdate(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onAfterUpdate';
 	}
 
 	/** Persistovane zmeny (insert) */
 	protected function onBeforeInsert(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onBeforeInsert';
 	}
 	/** Persistovane zmeny (insert) */
 	protected function onAfterInsert(IRepository $repository)
 	{
-		$this->checkEvent = true;
+		$this->checkEvent = 'onAfterInsert';
 	}
 
 	/**
@@ -88,7 +88,7 @@ abstract class _EntityEvent extends _EntityMeta
 			$entity->{$method}($repository, $id);
 		}
 
-		if ($entity->checkEvent !== true)
+		if ($entity->checkEvent !== $method)
 		{
 			$class = get_class($entity);
 			throw new InvalidStateException("Method $class::$method() or its descendant doesn't call parent::$method().");
