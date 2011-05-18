4acdd9a08d0f19a13a7cf56fa1b9ae868ba5070c
PetrP <prochazkapp@gmail.com>
2011-05-19 01:41:59 +0200

    Tests dump: pro Namespace Nette 2.0

--- tests/libs/dump.php
+++ tests/libs/dump.php
@@ -1,10 +1,12 @@
 <?php
 
+use \Nette\Diagnostics\Debugger;
+
 /** Dump */
 function d($var)
 {
 	if (func_num_args() > 1) $var = func_get_args();
-	return Debug::dump($var);
+	Debugger::dump($var);
 }
 
 /** Bar dump */
@@ -12,5 +14,5 @@ function dd($var)
 {
 	if (func_num_args() > 1) $var = func_get_args();
 	else if (is_array($var)) $var = array(NULL => $var);
-	return Debug::barDump($var);
+	Debugger::barDump($var);
 }
