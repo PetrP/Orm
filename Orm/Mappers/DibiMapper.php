c014d47954ca3f82d3f4f838b716c36c094440c0
PetrP <prochazkapp@gmail.com>
2011-05-20 05:43:14 +0200

    DibiPersistenceHelper: do samostatneho souboru

--- Orm/Mappers/DibiMapper.php
+++ Orm/Mappers/DibiMapper.php
@@ -5,17 +5,14 @@ namespace Orm;
 use Nette\NotImplementedException;
 use Nette\NotSupportedException;
 use Nette\InvalidStateException;
-use Nette\Object;
 use Dibi;
 use DibiConnection;
 use DibiTranslator;
-use DibiException;
-use ArrayObject;
-use DateTime;
 use ReflectionMethod;
 use stdClass;
 
 require_once dirname(__FILE__) . '/Mapper.php';
+require_once dirname(__FILE__) . '/DibiPersistenceHelper.php';
 require_once dirname(__FILE__) . '/Conventional/SqlConventional.php';
 require_once dirname(__FILE__) . '/Collection/DibiCollection.php';
 
@@ -335,102 +332,4 @@ class DibiMapper extends Mapper
 		}
 		return $result;
 	}
-}
-// todo refactor
-class DibiPersistenceHelper extends Object
-{
-	public $table;
-	public $connection;
-	public $conventional;
-	public $mapper;
-
-	public $witchParams = NULL;
-	public $witchParamsNot = NULL;
-
-	public function persist(IEntity $entity, $id = NULL)
-	{
-		$values = $entity->toArray();
-		if ($id !== NULL) $values['id'] = $id;
-
-		foreach ($values as $key => $value)
-		{
-			if ($key !== 'id' AND (($this->witchParams !== NULL AND !in_array($key, $this->witchParams)) OR ($this->witchParamsNot !== NULL AND in_array($key, $this->witchParamsNot))))
-			{
-				unset($values[$key]);
-				continue;
-			}
-			if ($value instanceof IEntityInjection)
-			{
-				$values[$key] = $value = $value->getInjectedValue();
-			}
-
-			if ($value instanceof IEntity)
-			{
-				$values[$key] = $value->id;
-			}
-			else if (is_array($value) OR ($value instanceof ArrayObject AND get_class($value) == 'ArrayObject'))
-			{
-				$values[$key] = serialize($value); // todo zkontrolovat jestli je jednodimenzni a neobrahuje zadne nesmysly
-			}
-			else if ($value instanceof IRelationship)
-			{
-				unset($values[$key]);
-			}
-			else if (is_object($value) AND method_exists($value, '__toString'))
-			{
-				$values[$key] = $value->__toString();
-			}
-			else if ($value !== NULL AND !($value instanceof DateTime) AND !is_scalar($value))
-			{
-				throw new InvalidStateException("Neumim ulozit `".get_class($entity)."::$$key` " . (is_object($value) ? get_class($value) : gettype($value)));
-			}
-		}
-
-		$values = $this->conventional->formatEntityToStorage($values);
-		$table = $this->table;
-
-		if (method_exists($this->connection->driver, 'getReflector'))
-		{
-			$columns = $this->connection->driver->getReflector()->getColumns($table);
-		}
-		else
-		{
-			$columns = $this->connection->driver->getColumns($table);
-		}
-		// todo inline cache
-
-		$tmp = array();
-		foreach ($columns as $column)
-		{
-			if (array_key_exists($column['name'], $values))
-			{
-				$tmp[$column['name']] = $values[$column['name']];
-			}
-			// todo else nejaky zpusob jak rict o chybe, protoze nekdy to chyba byt muze, jindy ale ne
-		}
-		// todo dalsi ktere nejsou v tabulce muze byt taky chyba (ale nemusi)
-		// todo vytvorit moznost zkontrolovat db, kde se budou kontrolovat jestli nejaky radek nechybi, nebo naopak nepribyva, jestli nekte nechyby NULL (nebo naopak), mozna i zkontrolovat default hodnoy, a typy
-		$values = $tmp;
-
-		if (isset($entity->id) AND $this->connection->fetch('SELECT [id] FROM %n WHERE [id] = %s', $table, $entity->id))
-		{
-			$id = $entity->id;
-			$this->connection->update($table, $values)->where('[id] = %s', $id)->execute();
-		}
-		else
-		{
-			if (array_key_exists('id', $values) AND $values['id'] === NULL) unset($values['id']);
-			$this->connection->insert($table, $values)->execute();
-			try {
-				$id = $this->connection->getInsertId();
-			} catch (DibiException $e) {
-				if (isset($values['id'])) $id = $values['id'];
-				else if (isset($entity->id)) $id = $entity->id;
-				else throw $e;
-			}
-		}
-
-		return $id;
-	}
-
 }
