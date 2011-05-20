a8aaa52e60f2c214d83ebccd629c78ce0dfb0c6a
PetrP <prochazkapp@gmail.com>
2011-05-20 02:27:58 +0200

    Tests: zapnuto zobrazovani struktury

--- tests/run.php
+++ tests/run.php
@@ -4,5 +4,6 @@ require_once dirname(__FILE__) . '/libs/Nette/loader.php';
 require_once dirname(__FILE__) . '/libs/HttpPHPUnit/init.php';
 
 $http = new HttpPHPUnit;
+$http->structure();
 $http->coverage(dirname(__FILE__) . '/../Orm', dirname(__FILE__) . '/report');
 $http->run(dirname(__FILE__) . '/unit');
