88b9b6ef3d1e3837151c9b912d2de109dba65b70
PetrP <prochazkapp@gmail.com>
2011-08-12 12:08:27 +0200

    Soubor Orm.php se loaduje vzdy

--- Orm/RepositoryContainer/RepositoryContainer.php
+++ Orm/RepositoryContainer/RepositoryContainer.php
@@ -12,6 +12,7 @@ use Nette\InvalidStateException;
 use Nette\DeprecatedException;
 use ReflectionClass;
 
+require_once __DIR__ . '/../Orm.php';
 require_once __DIR__ . '/IRepositoryContainer.php';
 require_once __DIR__ . '/../Entity/Entity.php';
 require_once __DIR__ . '/../Repository/Repository.php';
