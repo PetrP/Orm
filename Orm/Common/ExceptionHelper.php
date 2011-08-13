563dbedb54b4de4eac7f885b5bc9cbd35e5b3236
PetrP <prochazkapp@gmail.com>
2011-08-13 04:00:16 +0200

    ExceptionHelper: pro automatizaci exception textu

---
+++ Orm/Common/ExceptionHelper.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Orm
+ * @author Petr Procházka (petr@petrp.cz)
+ * @license "New" BSD License
+ */
+
+namespace Orm;
+
+class ExceptionHelper
+{
+
+	/**
+	 * @param string|array
+	 * @param string
+	 * @return string
+	 */
+	public static function format($message, $format)
+	{
+		if (is_array($message))
+		{
+			$data = array_merge(array(NULL), array_values($message)); unset($data[0]);
+			while (preg_match('#<%([0-9&!\|]+)%([^<>]*)>#is', $format))
+			$format = preg_replace_callback('#<%([0-9&!\|]+)%([^<>]*)>#is', function ($m) use ($data) {
+				foreach (explode('&', $m[1]) as $i)
+				{
+					$r = false;
+					foreach (explode('|', $i) as $i)
+					{
+						$neg = $i{0} === '!';
+						if (($neg AND empty($data[ltrim($i, '!')])) OR (!$neg AND !empty($data[$i])))
+						{
+							$r = true;
+							break;
+						}
+					}
+					if (!$r) return NULL;
+				}
+				return $m[2];
+			}, $format);
+			$format = preg_replace_callback('#%(.)([0-9])#si', function ($m) use ($data) {
+				return call_user_func(array('Orm\ExceptionHelper', $m[1]), isset($data[$m[2]]) ? $data[$m[2]] : NULL);
+			}, $format);
+			$message = $format;
+		}
+		return $message;
+	}
+
+	/**
+	 * @param string|object
+	 * @return string
+	 */
+	public static function c($class)
+	{
+		return is_object($class) ? get_class($class) : $class;
+	}
+
+	/**
+	 * @param string
+	 * @return string
+	 */
+	public static function s($string)
+	{
+		return $string;
+	}
+
+	/**
+	 * @param mixed
+	 * @return string
+	 */
+	public static function t($type)
+	{
+		return is_object($type) ? get_class($type) : gettype($type);
+	}
+
+}
---
+++ tests/cases/Common/ExceptionHelper_Test.php
@@ -0,0 +1,92 @@
+<?php
+
+use Orm\ExceptionHelper;
+
+/**
+ * @covers Orm\ExceptionHelper
+ */
+class ExceptionHelper_Test extends TestCase
+{
+	public function testNotArray()
+	{
+		$m = ExceptionHelper::format('ss', '');
+		$this->assertSame('ss', $m);
+	}
+
+	public function testString()
+	{
+		$this->assertSame('ss', ExceptionHelper::format(array('ss'), '%s1'));
+	}
+
+	public function testClass()
+	{
+		$this->assertSame('ss', ExceptionHelper::format(array('ss'), '%c1'));
+		$this->assertSame('ArrayObject', ExceptionHelper::format(array(new ArrayObject), '%c1'));
+	}
+
+	public function testType()
+	{
+		$this->assertSame('string', ExceptionHelper::format(array('ss'), '%t1'));
+		$this->assertSame('ArrayObject', ExceptionHelper::format(array(new ArrayObject), '%t1'));
+		$this->assertSame('integer', ExceptionHelper::format(array(123), '%t1'));
+		$this->assertSame('NULL', ExceptionHelper::format(array(NULL), '%t1'));
+	}
+
+	public function testMore()
+	{
+		$this->assertSame('a b c', ExceptionHelper::format(array('a', 'b', 'c'), '%s1 %s2 %s3'));
+	}
+
+	public function testIf()
+	{
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true), 'a<%1%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false), 'a<%1%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(''), 'a<%1%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(NULL), 'a<%1%bbb>c'));
+	}
+
+	public function testIfOr()
+	{
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, true), 'a<%1|2%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, false), 'a<%1|2%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(false, true), 'a<%1|2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, false), 'a<%1|2%bbb>c'));
+	}
+
+	public function testIfAnd()
+	{
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, true), 'a<%1&2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(true, false), 'a<%1&2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, true), 'a<%1&2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, false), 'a<%1&2%bbb>c'));
+	}
+
+	public function testIfAndOr()
+	{
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, true, true), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, true, false), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, false, true), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(true, false, false), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, true, true), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, true, false), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, false, true), 'a<%1&2|3%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, false, false), 'a<%1&2|3%bbb>c'));
+	}
+
+	public function testIfNot()
+	{
+		$this->assertSame('ac', ExceptionHelper::format(array(true), 'a<%!1%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(false), 'a<%!1%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(), 'a<%!1%bbb>c'));
+
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, false), 'a<%1&!2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, false), 'a<%1&!2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, true), 'a<%1&!2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(true, true), 'a<%1&!2%bbb>c'));
+
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, false), 'a<%1|!2%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(false, false), 'a<%1|!2%bbb>c'));
+		$this->assertSame('ac', ExceptionHelper::format(array(false, true), 'a<%1|!2%bbb>c'));
+		$this->assertSame('abbbc', ExceptionHelper::format(array(true, true), 'a<%1|!2%bbb>c'));
+	}
+}
