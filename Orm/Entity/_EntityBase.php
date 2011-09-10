b0fba756cb65c02104d457e15ba7f81be7ba27ec
PetrP <prochazkapp@gmail.com>
2011-09-10 05:56:20 +0100

    BaseEntityFragment: setValues nepouziva nette reflexi

--- Orm/Entity/_EntityBase.php
+++ Orm/Entity/_EntityBase.php
@@ -9,6 +9,8 @@ namespace Orm;
 
 use ArrayIterator;
 use Nette\NotSupportedException;
+use ReflectionMethod;
+use ReflectionProperty;
 
 /**
  * Hromadne nastavovani dat (setValues), prevod na pole (toArray),
@@ -52,7 +54,7 @@ abstract class _EntityBase extends _EntityValue
 			}
 			else if (method_exists($this, "set$name"))
 			{
-				$r = $this->getReflection()->getMethod("set$name");
+				$r = new ReflectionMethod($this, "set$name");
 				if ($r->isPublic())
 				{
 					$r->invoke($this, $value);
@@ -60,7 +62,7 @@ abstract class _EntityBase extends _EntityValue
 			}
 			else if (property_exists($this, $name))
 			{
-				$r = $this->getReflection()->getProperty($name);
+				$r = new ReflectionProperty($this, $name);
 				if ($r->isPublic())
 				{
 					$r->setValue($this, $value);
