0b24a22feeb39f36757d63439158adda29c960fd
PetrP <prochazkapp@gmail.com>
2011-10-21 19:16:36 +0200

    Tests: Events typo

--- tests/cases/Repository/Events/Events/Events_construct_Test.php
+++ tests/cases/Repository/Events/Events/Events_construct_Test.php
@@ -28,21 +28,50 @@ class Events_construct_Test extends TestCase
 
 	public function testConstancts()
 	{
-		$this->assertSame(pow(2, 0), Events::LOAD_BEFORE);
-		$this->assertSame(pow(2, 1), Events::LOAD_AFTER);
-		$this->assertSame(pow(2, 2), Events::ATTACH);
-		$this->assertSame(pow(2, 3), Events::PERSIST_BEFORE);
-		$this->assertSame(pow(2, 4), Events::PERSIST_BEFORE_INSERT);
-		$this->assertSame(pow(2, 5), Events::PERSIST_BEFORE_UPDATE);
-		$this->assertSame(pow(2, 6), Events::PERSIST);
-		$this->assertSame(pow(2, 7), Events::PERSIST_AFTER_INSERT);
-		$this->assertSame(pow(2, 8), Events::PERSIST_AFTER_UPDATE);
-		$this->assertSame(pow(2, 9), Events::PERSIST_AFTER);
-		$this->assertSame(pow(2, 10), Events::REMOVE_BEFORE);
-		$this->assertSame(pow(2, 11), Events::REMOVE_AFTER);
-		$this->assertSame(pow(2, 12), Events::FLUSH_BEFORE);
-		$this->assertSame(pow(2, 13), Events::FLUSH_AFTER);
-		$this->assertSame(pow(2, 14), Events::CLEAN_BEFORE);
-		$this->assertSame(pow(2, 15), Events::CLEAN_AFTER);
+		$uses = array('a' => array(), 'i' => 0, 'c' => 0);
+
+		$this->checkConstant(pow(2, 0), Events::LOAD_BEFORE, $uses);
+		$this->checkConstant(pow(2, 1), Events::LOAD_AFTER, $uses);
+		$this->checkConstant(pow(2, 2), Events::ATTACH, $uses);
+		$this->checkConstant(pow(2, 3), Events::PERSIST_BEFORE, $uses);
+		$this->checkConstant(pow(2, 4), Events::PERSIST_BEFORE_INSERT, $uses);
+		$this->checkConstant(pow(2, 5), Events::PERSIST_BEFORE_UPDATE, $uses);
+		$this->checkConstant(pow(2, 6), Events::PERSIST, $uses);
+		$this->checkConstant(pow(2, 7), Events::PERSIST_AFTER_INSERT, $uses);
+		$this->checkConstant(pow(2, 8), Events::PERSIST_AFTER_UPDATE, $uses);
+		$this->checkConstant(pow(2, 9), Events::PERSIST_AFTER, $uses);
+		$this->checkConstant(pow(2, 10), Events::REMOVE_BEFORE, $uses);
+		$this->checkConstant(pow(2, 11), Events::REMOVE_AFTER, $uses);
+		$this->checkConstant(pow(2, 12), Events::FLUSH_BEFORE, $uses);
+		$this->checkConstant(pow(2, 13), Events::FLUSH_AFTER, $uses);
+		$this->checkConstant(pow(2, 14), Events::CLEAN_BEFORE, $uses);
+		$this->checkConstant(pow(2, 15), Events::CLEAN_AFTER, $uses);
+
+		$r = new ReflectionClass('Orm\Events');
+		$c = $r->getConstants();
+		$this->assertSame($uses['c'], count($c));
+		foreach ($c as $key => $value)
+		{
+			if (!isset($uses['a'][$value]))
+			{
+				$this->fail('a');
+			}
+		}
+	}
+
+	private function checkConstant($int, $const, array & $uses)
+	{
+		$this->assertSame($int, $const);
+		if (isset($uses['a'][$const]))
+		{
+			$this->fail('a');
+		}
+		if ($uses['i'] & $const)
+		{
+			$this->fail('i');
+		}
+		$uses['a'][$const] = $const;
+		$uses['i'] |= $const;
+		$uses['c']++;
 	}
 }
