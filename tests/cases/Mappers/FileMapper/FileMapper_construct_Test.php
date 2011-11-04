d553bd12f408640df10551c3157c23e6303aa2d1
PetrP <prochazkapp@gmail.com>
2011-11-04 11:14:16 +0100

    Tests: type; starsi nette nema automaticky zaregistrovanej SafeStream

--- tests/cases/Mappers/FileMapper/FileMapper_construct_Test.php
+++ tests/cases/Mappers/FileMapper/FileMapper_construct_Test.php
@@ -21,7 +21,11 @@ class FileMapper_construct_Test extends TestCase
 	public function test()
 	{
 		$r = new TestsRepository(new RepositoryContainer);
-		stream_wrapper_unregister(SafeStream::PROTOCOL);
+		$wrapers = stream_get_wrappers();
+		if (in_array(SafeStream::PROTOCOL, $wrapers, true))
+		{
+			stream_wrapper_unregister(SafeStream::PROTOCOL);
+		}
 		SafeStream::register();
 
 		$this->assertContains(SafeStream::PROTOCOL, stream_get_wrappers());
@@ -42,14 +46,18 @@ class FileMapper_construct_Test extends TestCase
 	public function testDisabled()
 	{
 		$r = new TestsRepository(new RepositoryContainer);
-		stream_wrapper_unregister(SafeStream::PROTOCOL);
+		$wrapers = stream_get_wrappers();
+		if (in_array(SafeStream::PROTOCOL, $wrapers, true))
+		{
+			stream_wrapper_unregister(SafeStream::PROTOCOL);
+		}
 		$this->assertNotContains(SafeStream::PROTOCOL, stream_get_wrappers());
 		if ($this->readAttribute('Orm\FileMapper', 'isStreamRegistered')) // php52 projde pri samostanem testu
 		{
 			setAccessible(new ReflectionProperty('Orm\FileMapper', 'isStreamRegistered'))->setValue(false);
 		}
 		$this->assertAttributeSame(false, 'isStreamRegistered', 'Orm\FileMapper');
-		$this->setExpectedException('Orm\NotSupportedException', "Stream 'safe' is not registered; use Nette\\Utils\\SafeStream::register().");
+		$this->setExpectedException('Orm\NotSupportedException', "Stream 'safe' is not registered; use Nette" . "\\Utils\\SafeStream::register().");
 		new FileMapper_FileMapper($r);
 	}
 
