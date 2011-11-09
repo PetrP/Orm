843da971682ac19070f5595580b9e56fe7f6a022
PetrP <prochazkapp@gmail.com>
2011-11-10 00:33:21 +0100

    Builder: parametr full zrusen, rozeznava se jestli je udana verze

--- Builder/run.php
+++ Builder/run.php
@@ -5,10 +5,9 @@ namespace Orm\Builder;
 require_once __DIR__ . '/inc/boot.php';
 
 $isDev = isset($_GET['dev']);
-$full = isset($_GET['full']) ? true : !$isDev;
 $info = new VersionInfo(new Git(__DIR__ . '/..'), $isDev, $isDev ? $_GET['dev'] : NULL);
 
-$zip = new Zipper(__DIR__ . "/Orm-{$info->tag}.zip", __DIR__, $full);
+$zip = new Zipper(__DIR__ . "/Orm-{$info->tag}.zip", __DIR__, $info->versionId !== -1);
 
 $b = new Builder(Builder::NS | Builder::NS_NETTE, $info);
 $b->build(__DIR__ . "/../Orm", __DIR__ . "/php53/Orm");
@@ -18,7 +17,7 @@ $b = new Builder(Builder::NONNS | Builder::NONNS_NETTE, $info);
 $b->build(__DIR__ . "/../Orm", __DIR__ . "/php52/Orm");
 $zip->add($b);
 
-if ($full)
+if ($info->versionId !== -1)
 {
 	$api = new Api;
 	$api->generate(__DIR__ . "/php52/Orm", __DIR__ . "/php52/Api");
@@ -29,7 +28,7 @@ if ($full)
 
 $zip->save();
 
-echo "<h1>{$info->tag}<h1>{$info->shortSha}<br>{$info->versionId}" . ($full ? '<br>full' : '');
+echo "<h1>{$info->tag}<h1>{$info->shortSha}<br>{$info->versionId}";
 
 foreach (array(
 	Builder::NS | Builder::NS_NETTE => 'php53/Nette_with_namespaces',
