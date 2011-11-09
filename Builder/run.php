2c07bf1990f58d28d42c920c12f811404bb542d2
PetrP <prochazkapp@gmail.com>
2011-11-09 23:46:12 +0100

    Builder: zprovozneno testovaci prostredi

--- Builder/run.php
+++ Builder/run.php
@@ -2,22 +2,7 @@
 
 namespace Orm\Builder;
 
-require_once __DIR__ . '/libs/nette.min.php';
-require_once __DIR__ . '/../tests/libs/dump.php';
-
-use Nette\Diagnostics\Debugger;
-use Nette\Loaders\RobotLoader;
-use Nette\Caching\Storages\FileStorage;
-
-Debugger::enable();
-Debugger::$strictMode = true;
-set_time_limit(0);
-
-$r = new RobotLoader;
-$r->setCacheStorage(new FileStorage(__DIR__ . '/../tests/tmp'));
-$r->addDirectory(__DIR__ . '/inc');
-$r->addDirectory(__DIR__ . '/libs');
-$r->register();
+require_once __DIR__ . '/inc/boot.php';
 
 $isDev = isset($_GET['dev']);
 $full = isset($_GET['full']) ? true : !$isDev;
