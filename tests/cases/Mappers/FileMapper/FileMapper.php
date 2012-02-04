20eebbfbdd0d6db3a1645de7440a7e57abef2da5
PetrP <prochazkapp@gmail.com>
2012-02-04 21:53:17 +0100

    Tests: FileMapper zabranuje kolizim, pri vice vlaknech

--- tests/cases/Mappers/FileMapper/FileMapper.php
+++ tests/cases/Mappers/FileMapper/FileMapper.php
@@ -5,10 +5,19 @@ use Orm\RepositoryContainer;
 
 class FileMapper_FileMapper extends FileMapper
 {
+	private $file;
 
 	protected function getFilePath()
 	{
-		return realpath(sys_get_temp_dir()) . '/OrmTest_' . __CLASS__ . '.data';
+		if (!$this->file)
+		{
+			$this->file = $file = realpath(tempnam(sys_get_temp_dir(), __CLASS__));
+			@unlink($file);
+			register_shutdown_function(function () use ($file) {
+				@unlink($file);
+			});
+		}
+		return $this->file;
 	}
 
 	public function _loadData()
@@ -33,6 +42,5 @@ abstract class FileMapper_Base_Test extends TestCase
 	protected function setUp()
 	{
 		$this->m = new FileMapper_FileMapper(new TestsRepository(new RepositoryContainer));
-		@unlink($this->m->_getFilePath());
 	}
 }
