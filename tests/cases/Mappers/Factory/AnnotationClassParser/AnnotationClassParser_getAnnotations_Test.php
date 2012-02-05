ea462138e6e0706a31cdf8034c93235a932cbc91
PetrP <prochazkapp@gmail.com>
2012-02-05 03:32:32 +0100

    Tests: AnnotationsParser v php52 nette obsahuje i package

--- tests/cases/Mappers/Factory/AnnotationClassParser/AnnotationClassParser_getAnnotations_Test.php
+++ tests/cases/Mappers/Factory/AnnotationClassParser/AnnotationClassParser_getAnnotations_Test.php
@@ -15,8 +15,15 @@ class AnnotationClassParser_getAnnotations_Test extends TestCase
 	public function test()
 	{
 		$a = $this->p->_getAnnotations(new ReflectionClass('Nette\Reflection\AnnotationsParser'));
-		$this->assertSame(3, count($a));
-		$this->assertSame(array('Annotations support for PHP.'), $a['description']);
+		$this->assertSame(isset($a['description']) ? (isset($a['package']) ? 4 : 3) : 2, count($a));
+		if (isset($a['description'])) // jen v novejsich nette
+		{
+			$this->assertSame(array('Annotations support for PHP.'), $a['description']);
+		}
+		if (isset($a['package'])) // jen v php52 nette
+		{
+			$this->assertSame(array('Nette' . '\\' . 'Reflection'), $a['package']);
+		}
 		$this->assertSame(array('David Grudl'), $a['author']);
 		$this->assertSame(array(true), $a['Annotation']);
 	}
