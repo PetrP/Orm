27b697a38033ad6d7a460901c6d6b3565f5455af
PetrP <prochazkapp@gmail.com>
2013-04-22 16:13:08 +0200

    Tests\Loader: pridan Access 0.2

--- tests/loader.php
+++ tests/loader.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/libs/Nette/loader.php';
 require_once __DIR__ . '/libs/dump.php';
 require_once __DIR__ . '/libs/dibi/dibi.php';
 require_once __DIR__ . '/libs/HttpPHPUnit/ResultPrinter/NetteDebug.php';
+require_once __DIR__ . '/libs/Access/Init.php';
 
 use Nette\Environment;
 use Nette\Loaders\RobotLoader;
