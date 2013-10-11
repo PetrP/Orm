87616df3dfdec14b1dbeea6bcb499b7d05371d47
PetrP <prochazkapp@gmail.com>
2013-10-11 12:33:13 +0200

    Fixed bug: "Interface 'Orm\IEntity' not found" (d1f6b413)
    Pri pouziti Nette\Loaders\RobotLoader porad existovali situace kdy slo na tento bug narazit.
    Nove kdyz se na jednu tridu pouzije vlastní loader tak se na vsechny tridy musi pouzit.
    Orm.php pak nedela require vsech souboru.

--- Orm/RepositoryContainer/IRepositoryContainer.php
+++ Orm/RepositoryContainer/IRepositoryContainer.php
@@ -58,5 +58,3 @@ interface IRepositoryContainer
 	public function getContext();
 
 }
-
-require_once __DIR__ . '/../Orm.php';
---
+++ tests/cases/Orm/Orm_require_Test.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @covers Orm\Orm
+ */
+class Orm_require_Test extends TestCase
+{
+
+	/**
+	 * @runInSeparateProcess
+	 * @dataProvider dataProviderClasses
+	 */
+	public function test($class, $path, $type)
+	{
+		$this->assertFalse(class_exists('Orm\Orm', false));
+		$this->assertFalse($type($class, false)); // interface_exists or class_exists
+
+		require_once $path;
+
+		$this->assertTrue($type($class, false)); // interface_exists or class_exists
+
+		if (stripos($class, 'Exception') === false AND $type !== 'interface_exists')
+		{
+			$ignore = array(
+				'Orm\ObjectMixin' => true,
+				'Orm\EntityHelper' => true,
+				'Orm\ValidationHelper' => true,
+				'Orm\InjectionFactory' => true,
+				'Orm\HydrateEntityIterator' => true,
+				'Orm\FetchAssoc' => true,
+				'Orm\FindByHelper' => true,
+			);
+			if (!isset($ignore[$class]))
+			{
+				$this->assertTrue(class_exists('Orm\Orm', false));
+			}
+		}
+	}
+
+	public function dataProviderClasses()
+	{
+		global $ormDir;
+		if (!isset($ormDir)) $_ormDir = __DIR__ . '/../../../Orm';
+		else $_ormDir = $ormDir;
+		$rpOrmDir = realpath($_ormDir);
+		$rpOrmDirLen = strlen($rpOrmDir);
+		$this->assertTrue($rpOrmDir AND $_ormDir);
+		$result = array();
+		foreach (array_merge(get_declared_classes(), get_declared_interfaces()) as $class)
+		{
+			$r = new ReflectionClass($class);
+			if ($path = $r->getFileName())
+			{
+				$path = realpath($path);
+				if (strncmp($path, $rpOrmDir, $rpOrmDirLen) === 0)
+				{
+					$result[] = array($class, $path, $r->isInterface() ? 'interface_exists' : 'class_exists');
+				}
+			}
+		}
+		return $result;
+	}
+
+}
