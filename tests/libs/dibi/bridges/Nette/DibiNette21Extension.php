89293da260f175802db7b033fe6769f8dde745c9
PetrP <prochazkapp@gmail.com>
2014-02-05 18:52:58 +0100

    Libs: update Dibi 2.1.1

--- tests/libs/dibi/bridges/Nette/DibiNette21Extension.php
+++ tests/libs/dibi/bridges/Nette/DibiNette21Extension.php
@@ -10,7 +10,6 @@
  */
 
 
-
 /**
  * Dibi extension for Nette Framework 2.1. Creates 'connection' service.
  *
@@ -28,7 +27,7 @@ class DibiNette21Extension extends Nette\DI\CompilerExtension
 
 		$useProfiler = isset($config['profiler'])
 			? $config['profiler']
-			: !$container->parameters['productionMode'];
+			: $container->parameters['debugMode'];
 
 		unset($config['profiler']);
 
