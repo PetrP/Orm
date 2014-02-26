cc59fd3a6820d2536e3d5693dabaa5232aff0454
PetrP <prochazkapp@gmail.com>
2014-02-26 02:49:06 +0100

    Tests: PhpVersionIdTest vyjimka pro php 5.4.6 ktere obsahuje bug
    Testujou se i ostatni konstanty
    aby se na podobne veci v budoucich verzich pravdepodobneji prislo.

--- tests/cases/Orm/PhpVersionIdTest.php
+++ tests/cases/Orm/PhpVersionIdTest.php
@@ -5,7 +5,21 @@ class PhpVersionIdTest extends TestCase
 	public function test()
 	{
 		$this->assertTrue(defined('PHP_VERSION_ID'));
+		$this->assertSame(PHP_VERSION, phpversion());
+		if (PHP_VERSION === '5.4.6' AND PHP_VERSION_ID === 50405)
+		{
+			// V 5.4.6 zapomeli zmenit PHP_VERSION_ID.
+			// https://github.com/php/php-src/commit/3548b2e2c5bbe361c3c95a4df563e81f33da4b7e#diff-6b877bd60916a7ba8fd1c6028b1b28aaR8
+			$this->assertSame('5.4.6', PHP_VERSION);
+			$this->assertSame('5.4.6', implode('.', array(PHP_MAJOR_VERSION, PHP_MINOR_VERSION, PHP_RELEASE_VERSION)));
+			$this->assertSame(50405, PHP_VERSION_ID);
+			$this->markTestSkipped('php 5.4.6 bug');
+		}
 		$tmp = explode('.', PHP_VERSION);
 		$this->assertSame(($tmp[0] * 10000 + $tmp[1] * 100 + $tmp[2]), PHP_VERSION_ID);
+		if (PHP_VERSION_ID >= 50207)
+		{
+			$this->assertSame((PHP_MAJOR_VERSION * 10000 + PHP_MINOR_VERSION * 100 + PHP_RELEASE_VERSION), PHP_VERSION_ID);
+		}
 	}
 }
