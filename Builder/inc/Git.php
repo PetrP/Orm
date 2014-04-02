eb6d270e7d2b9581eb2cbb47b7eea6e8c6797b82
PetrP <prochazkapp@gmail.com>
2014-04-02 21:56:49 +0200

    Builder\Git: fixed bug; getSha ma vracet plny hash ne zkraceny (a08e7ab4)

--- Builder/inc/Git.php
+++ Builder/inc/Git.php
@@ -74,7 +74,7 @@ class Git extends Object
 
 	public function getSha($input)
 	{
-		return trim($this->command('rev-parse --short ' . $this->escape($input)));
+		return trim($this->command('rev-parse ' . $this->escape($input)));
 	}
 
 	/**
